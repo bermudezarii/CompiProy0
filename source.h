e0fa434dc5300185f1ef04a46780c2143add32e9
bermudezarii <bermudezarii@gmail.com>
2017-02-18 01:15:10 -0600

    Copy de la lectura
    Codigo (:

---
+++ source.h
@@ -0,0 +1,8 @@
+typedef enum token_types
+{
+	BEGIN, END, READ, WRITE, ID, INTLITERAL, 
+	LPAREN, RPAREN, SEMICOLON, COMMA, ASSIGNOP, 
+	PLUSOP, MINUSOP, SCANEOF
+}; token; 
+
+extern token scanner(void); 
